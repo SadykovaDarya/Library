1e01fb134d0364f522c1eafd8995da4b068d664f
ДИТ - Отдел поддержки пользователей НИУ ВШЭ <ditsupport@hse.ru>
2017-11-03 14:51:39 +0300

    Third vrsion

--- LibraryUI/MainWindow.xaml.cs
+++ LibraryUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace LibraryUI
         public MainWindow()
         {
             InitializeComponent();
+
+            using (var context = new Context())
+            {
+                context.Books.ToList();
+            }
         }
     }
 }
